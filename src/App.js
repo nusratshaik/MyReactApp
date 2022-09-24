3b0ad151b6c2190edf075bc8582bcda829479d73
nusratshaik <96132929+nusratshaik@users.noreply.github.com>
2022-09-24 09:12:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./Components/styles.css";
+import ChangeColors from "./Components/ChangeColors";
+function App() {
+  return (
+    <div style={{ width: "90%", margin: "0 auto" }}>
+      <ChangeColors />
+    </div>
+  );
+}
+export default App;
